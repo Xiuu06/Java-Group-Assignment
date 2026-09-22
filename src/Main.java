436f16e4f05fa8cf02adced50fe1541453f18e19
Xiuu <Xiuu091@gmail.com>
2026-09-22 20:52:26 +0800

    Main Changing

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Staff staff = new Staff(101, "John", "john@gmail.com");
-        Admin admin = new Admin(201, "Alice", "alice@gmail.com");
-        Customer customer = new Customer(301, "David", "david@gmail.com");
+        User[] users = new User[3];
 
-        staff.displayStaff();
+        users[0] = new Staff(101, "John", "john@gmail.com");
+        users[1] = new Admin(201, "Alice", "alice@gmail.com");
+        users[2] = new Customer(301, "David", "david@gmail.com");
 
-        System.out.println();
-
-        admin.displayAdmin();
-
-        System.out.println();
-
-        customer.displayCustomer();
+        for (int i = 0; i < users.length; i++) {
+            System.out.println("User " + (i + 1) + " Role: " + users[i].getRole());
+        }
     }
 }
